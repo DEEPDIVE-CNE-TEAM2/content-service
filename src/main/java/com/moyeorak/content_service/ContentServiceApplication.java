ebc18754b7809e7a8bde53cb0da901f0d57fe3f6
LEE EUNJIN <127672852+leeenzang@users.noreply.github.com>
2025-09-06 11:28:04 +0900

    Feature: region (#1)

--- src/main/java/com/moyeorak/content_service/ContentServiceApplication.java
+++ src/main/java/com/moyeorak/content_service/ContentServiceApplication.java
@@ -1,9 +1,11 @@
 package com.moyeorak.content_service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RequiredArgsConstructor
 public class ContentServiceApplication {
 
 	public static void main(String[] args) {
